2b678bb956d279cf23b470a6ed8d249df02e15c5
ckaadic <ckaadic@gmail.com>
2022-08-21 19:22:04 +0200

    test

--- ckaadic.c
+++ ckaadic.c
@@ -81,10 +81,6 @@ bool process_record_user(uint16_t const keycode, keyrecord_t *record) {
             }
             break;
     }
-    return process_record_keymap(keycode, record);
-	//return true;
-}
-
-bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
-  return true;
+    //return process_record_keymap(keycode, record);
+	return true;
 }
