981dae2c68fdcd938d7bb6612846d1410aad193c
ckaadic <ckaadic@gmail.com>
2022-08-21 19:13:57 +0200

    test

--- ckaadic.c
+++ ckaadic.c
@@ -67,23 +67,28 @@ bool process_record_user(uint16_t const keycode, keyrecord_t *record) {
 	return true;
 }
 
-bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
+__attribute__ ((weak))
+bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
+  return true;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch(keycode) {
         case M_AE:
             if (record->event.pressed) {
                 SEND_STRING("\"a");
             }
-            return false;
+            break;
         case M_OE:
             if (record->event.pressed) {
                 SEND_STRING("\"o");
             }
-            return false;
+            break;
         case M_UE:
             if (record->event.pressed) {
                 SEND_STRING("\"u");
             }
-            return false;
+            break;
     }
-    return process_record_user(keycode, record);
+    return process_record_keymap(keycode, record);
 };
