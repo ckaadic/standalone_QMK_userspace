494646155efc7feb58a0163a601e727e63fc8d0f
ckaadic <ckaadic@gmail.com>
2022-08-22 18:59:05 +0200

    fixing oled

--- ckaadic.c
+++ ckaadic.c
@@ -47,19 +47,9 @@ bool process_record_user(uint16_t const keycode, keyrecord_t *record) {
 #ifdef TAPPING_TERM_PER_KEY
 		tap_timer = timer_read();
 #endif
-#if defined(OLED_ENABLE) && !defined(WPM_ENABLE)
-		extern uint32_t oled_tap_timer;
-		oled_tap_timer = timer_read32();
-#endif
 #ifdef AUTO_CORRECT
 		extern bool process_autocorrect(uint16_t keycode, keyrecord_t* record);
 		if (!process_autocorrect(keycode, record)) {
-			return false;
-		}
-#endif
-#ifdef CAPS_UNLOCK
-		extern bool process_caps_unlock(uint16_t keycode, keyrecord_t *record);
-		if (!process_caps_unlock(keycode, record)) {
 			return false;
 		}
 #endif
