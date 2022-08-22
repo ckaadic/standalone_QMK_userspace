3c2b1761fcde5afa620a65680fa41de858d33c94
ckaadic <ckaadic@gmail.com>
2022-08-22 14:00:51 +0200

    fix layout

--- keymaps/layout.h
+++ keymaps/layout.h
@@ -1,22 +1,5 @@
-// Copyright 2021 @filterpaper
-// SPDX-License-Identifier: GPL-2.0+
 
-// macOS shortcuts
-#define Z_UND	G(KC_Z)
-#define Z_CUT	G(KC_X)
-#define Z_CPY	G(KC_C)
-#define Z_PST	G(KC_V)
-#define Z_LOCK	C(G(KC_Q))
-#define Z_SLEEP	A(G(KC_PWR))
-#define Z_SHUT	C(A(G(KC_PWR)))
-#define Z_SSAVE	HYPR(KC_S)
 
-// Tap hold macros
-#define TH_M	LT(0,KC_M)
-#define TH_COMM	LT(0,KC_COMM)
-#define TH_DOT	LT(0,KC_DOT)
-#define TH_SLSH	LT(0,KC_SLSH)
-#define TH(k1,k2,k3,k4) LT(0,k1),LT(0,k2),LT(0,k3),LT(0,k4)
 
 // Home row mod-tap macros
 #define HM_A	LSFT_T(KC_A)
@@ -57,9 +40,9 @@
 
 // Default 3x6_3 split layout
 #define _BASE \
-	_______, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______, \
-	_______, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, _______, \
-	_______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______, \
+	KC_NO, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_NO, \
+	KC_NO, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_NO, \
+	KC_NO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_NO, \
 	                    KC_DEL, LT(SYM,KC_BSPC), OSM_SFT,     KC_SPC, LT(NUM,KC_TAB), KC_ENT
 /* ,-----------------------------------------.                ,-----------------------------------------.
    |      |  Q   |  W   |  E   |  R   |  T   |                |  Y   |  U   |  I   |  O   |  P   |      |
@@ -73,9 +56,9 @@
                                `--------------------'  `--------------------'
  */
 #define _COLE \
-	_______, KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,        KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT, _______, \
-	_______, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,        KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    _______, \
-	_______, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,        KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, _______, \
+	KC_NO, KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,        KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT, KC_NO, \
+	KC_NO, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,        KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_NO, \
+	KC_NO, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,        KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_NO, \
 	                    KC_DEL, LT(SYM,KC_BSPC), OSM_SFT,     KC_SPC, LT(NUM,KC_TAB), KC_ENT
 /* ,-----------------------------------------.                ,-----------------------------------------.
    |      |  Q   |  W   |  F   |  P   |  B   |                |  J   |  L   |  U   |  Y   | ' "  |      |
@@ -84,15 +67,15 @@
    |------+------+------+------+------+------|                |------+------+------+------+------+------|
    |      |  Z   |  X   |  C   |  D   |  V   |                |  K   |  H   | , <  | . >  | / ?  |      |
    `------+------+------+------+------+------+------.  ,------+------+------+------+------+------+------'
-                               | DEL  |_SYMB | OSM_ |  |RSFT_T|_NUMB | ENT  |
+                               | DEL |_SYMB | OSM_ |  |RSFT_T|_NUMB | ENT  |
                                |      | BSPC | SFT  |  | SPC  | TAB  |      |
                                `--------------------'  `--------------------'
  */
 #define _NUMB \
-	_______, KC_SCLN, KC_HASH, KC_AT,   KC_UNDS, KC_EXLM,     KC_ASTR, KC_MINS, KC_EQL,  KC_PLUS, KC_GRV,  _______, \
-	_______, KC_COLN, KC_BSLS, KC_LCBR, KC_LPRN, KC_LBRC,     KC_3,    KC_2,    KC_1,    KC_0,    KC_4,    _______, \
-	_______, KC_CIRC, KC_AMPR, KC_PERC, KC_DLR,  KC_DOT,      KC_7,    KC_6,    KC_5,    KC_9,    KC_8,    _______, \
-	                           _______, MO(FNC), KC_TRNS,     _______, _______, _______
+	KC_NO, KC_SCLN, KC_HASH, KC_AT,   KC_UNDS, KC_EXLM,     KC_ASTR, KC_MINS, KC_EQL,  KC_PLUS, KC_GRV,  KC_NO, \
+	KC_NO, KC_COLN, KC_BSLS, KC_LCBR, KC_LPRN, KC_LBRC,     KC_3,    KC_2,    KC_1,    KC_0,    KC_4,    KC_NO, \
+	KC_NO, KC_CIRC, KC_AMPR, KC_PERC, KC_DLR,  KC_DOT,      KC_7,    KC_6,    KC_5,    KC_9,    KC_8,    KC_NO, \
+	                           KC_NO, MO(FNC), KC_TRNS,     KC_NO, KC_NO, KC_NO
 /* ,-----------------------------------------.                ,-----------------------------------------.
    |      | INS  |  1   |  2   |  3   | VOLU |                | HOME | PGDN | PGUP | END  |  :   |      |
    |------+------+------+------+------+------|                |------+------+------+------+------+------|
@@ -104,10 +87,10 @@
                                `--------------------'  `--------------------'
  */
 #define _SYMB \
-	_______, KC_ESC,  _______,   _______, _______, _______,   _______, KC_PGUP, KC_UP,   _______, RALT(KC_S), _______, \
-	_______, OSM_GUI, OSM_ALT,   OSM_CTL, OSM_SFT, C(KC_S),   KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,     _______, \
-	_______, KC_ENT,  CAPS_WORD, C(KC_C), KC_DEL,  C(KC_V),   _______, KC_PGDN, _______, _______, _______,    _______, \
-	                             _______, _______, _______,   KC_TAB,  KC_ENT,  _______
+	KC_NO, KC_ESC,  KC_NO,   KC_NO, KC_NO, KC_NO,   KC_NO, KC_PGUP, KC_UP,   RALT(KC_S), KC_NO, KC_NO, \
+	KC_NO, OSM_GUI, OSM_ALT,   OSM_CTL, OSM_SFT, C(KC_S),   KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,     KC_NO, \
+	KC_NO, KC_ENT,  CAPS_WORD, C(KC_C), KC_DEL,  C(KC_V),   KC_NO, KC_PGDN, KC_NO, KC_NO, KC_NO,    KC_NO, \
+	                             KC_NO, KC_NO, KC_NO,   KC_TAB,  KC_ENT,  KC_NO
 /* ,-----------------------------------------.                ,-----------------------------------------.
    |      |      |  [   |  {   |  }   |      |                |  ^   |  (   |  )   |  ]   |  ~   |      |
    |------+------+------+------+------+------|                |------+------+------+------+------+------|
@@ -119,10 +102,10 @@
                                `--------------------'  `--------------------'
  */
 #define _FUNC \
-	_______, _______, _______, _______, _______,  _______,    KC_F12,  KC_F7,   KC_F8,   KC_F9,   _______, _______, \
-	_______, RGB_HUI, RGB_SAI, RGB_VAI, RGB_MOD,  RGB_TOG,    KC_F11,  KC_F4,   KC_F5,   KC_F6,   _______, _______, \
-	_______, RGB_HUD, RGB_SAD, RGB_VAD, RGB_RMOD, _______,    KC_F10,  KC_F1,   KC_F2,   KC_F3,   _______, _______, \
-	                           _______, _______, _______,     _______, _______, _______
+	KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,    KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_NO, KC_NO, \
+	KC_NO, RGB_HUI, RGB_SAI, RGB_VAI, RGB_MOD,  RGB_TOG,    KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_NO, KC_NO, \
+	KC_NO, RGB_HUD, RGB_SAD, RGB_VAD, RGB_RMOD, KC_NO,    KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_NO, KC_NO, \
+	                           KC_NO, KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO
 /* ,-----------------------------------------.                ,-----------------------------------------.
    |      |BOOT  |  F1  |  F2  |  F3  | F10  |                |      | WH_U | WH_D |      |_COLE |      |
    |------+------+------+------+------+------|                |------+------+------+------+------+------|
@@ -144,7 +127,7 @@
 ) \
 	k01,      k02, k03, k04, k05,  k06,    k07,      k08, k09, k10, k11,  k12, \
 	k13, HRML(k14, k15, k16, k17), k18,    k19, HRMR(k20, k21, k22, k23), k24, \
-	k25,      k26, k27, k28, k29,  k30,    k31,   TH(k32, k33, k34, k35), k36, \
+	k25,      k26, k27, k28, k29,  k30,    k31,      k32, k33, k34, k35,  k36, \
 	                    k37, k38,  k39,    k40, k41, k42
 
 
