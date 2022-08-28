243ba10cb5a7d40a821d76ccee50fc858cceaa80
ckaadic <ckaadic@gmail.com>
2022-08-28 20:22:30 +0200

    test

--- keymaps/layout.h
+++ keymaps/layout.h
@@ -164,28 +164,3 @@
 	     k26, k27, k28, k29, k30,    k31, k32, k33, k34, k35, \
 	                    k38, k39,    k40, k41
 
-const key_override_t lbracket_override = {.trigger_mods          = MOD_MASK_SHIFT,
-                                   .layers                 = ~0,
-                                   .suppressed_mods        = MOD_MASK_SHIFT,
-                                   .options                = ko_options_default,
-                                   .trigger                = KC_LBRACKET,
-                                   .replacement            = KC_RBRACKET,
-                                   .enabled                = NULL};
-
-const key_override_t hbracket_override = {.trigger_mods          = MOD_MASK_SHIFT,
-                                   .layers                 = ~0,
-                                   .suppressed_mods        = MOD_MASK_SHIFT,
-                                   .options                = ko_options_default,
-                                   .trigger                = LSFT(KC_LBRACKET),
-                                   .replacement            = LSFT(KC_RBRACKET),
-                                   .enabled                = NULL};
-
-const key_override_t bracket_override = ko_make_basic(MOD_MASK_SHIFT, LSFT(KC_9), LSFT(KC_0));
-
-const key_override_t **key_overrides = (const key_override_t *[]){
-    //&backspace_override,
-    &lbracket_override,
-    &hbracket_override,
-    &bracket_override,
-    NULL
-};
