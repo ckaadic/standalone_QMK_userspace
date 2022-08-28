e9922b61886f773f513d8828053cbcd3b2fa8131
ckaadic <ckaadic@gmail.com>
2022-08-28 21:01:26 +0200

    tt

--- ckaadic.c
+++ ckaadic.c
@@ -1,36 +1,7 @@
 #include "ckaadic.h"
 
 
-#ifdef TAPPING_TERM_PER_KEY
-#	define IS_TYPING() (timer_elapsed(tap_timer) < TAPPING_TERM * 1.3)
-static uint16_t tap_timer = 0;
-// Increase tapping term in between short key presses to avoid false trigger.
-uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
-	return MODTAP_BIT(keycode) & MOD_MASK_CAG && IS_TYPING() ? TAPPING_TERM * 1.3 : TAPPING_TERM;
-}
-#endif
 
-#ifdef PERMISSIVE_HOLD_PER_KEY
-// Select Shift mod tap immediately when another key is pressed and released.
-bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
-	return keycode == RSFT_T(KC_SPC) ? true : false;
-}
-#endif
-
-#ifdef HOLD_ON_OTHER_KEY_PRESS_PER_KEY
-// Select hold immediately with another key for layer tap 1 and higher.
-bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
-	return IS_LAYER_TAP(keycode) ? true : false;
-}
-#endif
-
-#ifdef TAPPING_FORCE_HOLD_PER_KEY
-// Force hold on tap-hold key when held after tapping,
-// enable for (right split) bottom row key.
-bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
-	return record->event.key.row == MATRIX_ROWS - 1 ? true : false;
-}
-#endif
 
 
 // Send custom hold keycode for mod tap.
@@ -49,8 +20,7 @@ bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
 
 bool process_record_user(uint16_t const keycode, keyrecord_t *record) {
 	switch (keycode) {
-#ifdef MACROS_ENABLED
-  case M_AE:
+        case M_AE:
             if (record->event.pressed) {
                 SEND_STRING("\"a");
             }
@@ -65,9 +35,33 @@ bool process_record_user(uint16_t const keycode, keyrecord_t *record) {
                 SEND_STRING("\"u");
             }
             return false;
-#endif
   }
 	return true;
 }
 
 
+const key_override_t lbracket_override = {.trigger_mods          = MOD_MASK_SHIFT,
+                                   .layers                 = ~0,
+                                   .suppressed_mods        = MOD_MASK_SHIFT,
+                                   .options                = ko_options_default,
+                                   .trigger                = KC_LBRACKET,
+                                   .replacement            = KC_RBRACKET,
+                                   .enabled                = NULL};
+
+const key_override_t hbracket_override = {.trigger_mods          = MOD_MASK_SHIFT,
+                                   .layers                 = ~0,
+                                   .suppressed_mods        = MOD_MASK_SHIFT,
+                                   .options                = ko_options_default,
+                                   .trigger                = LSFT(KC_LBRACKET),
+                                   .replacement            = LSFT(KC_RBRACKET),
+                                   .enabled                = NULL};
+
+const key_override_t bracket_override = ko_make_basic(MOD_MASK_SHIFT, LSFT(KC_9), LSFT(KC_0));
+
+const key_override_t **key_overrides = (const key_override_t *[]){
+    //&backspace_override,
+    &lbracket_override,
+    &hbracket_override,
+    &bracket_override,
+    NULL
+};
