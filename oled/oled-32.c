6b2e7a857d5ef79111aac6d7daf37947ffe7438c
ckaadic <ckaadic@gmail.com>
2022-09-15 20:59:19 +0200

    ttctrst

--- oled/oled-32.c
+++ oled/oled-32.c
@@ -40,6 +40,9 @@ void oled_render_logo(void) {
 }
 
 oled_rotation_t oled_init_kb(oled_rotation_t rotation) {
+    if (!is_keyboard_master()) {
+        return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+    }
     return OLED_ROTATION_270;
 }
 
