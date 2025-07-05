44ac945dc0118d124add93098fcbe7f33fcebd12
ckaadic <ckaadic@gmail.com>
2025-07-05 21:39:15 +0200

    remove legacy bracket override

--- ckaadic.c
+++ ckaadic.c
@@ -71,7 +71,7 @@ bool process_record_user(uint16_t const keycode, keyrecord_t *record) {
 }
 
 
-const key_override_t lbracket_override = {.trigger_mods          = MOD_MASK_SHIFT,
+/* const key_override_t lbracket_override = {.trigger_mods          = MOD_MASK_SHIFT,
                                    .layers                 = ~0,
                                    .suppressed_mods        = MOD_MASK_SHIFT,
                                    .options                = ko_options_default,
@@ -95,7 +95,7 @@ const key_override_t **key_overrides = (const key_override_t *[]){
     &hbracket_override,
     &bracket_override,
     NULL
-};
+}; */
 
 #ifdef ENCODER_ENABLE
 
