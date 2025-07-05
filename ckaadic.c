87554f845f1ee582ef1d9b121725abb0513c4a93
ckaadic <ckaadic@gmail.com>
2025-07-06 00:47:17 +0200

    include combos

--- ckaadic.c
+++ ckaadic.c
@@ -1,4 +1,7 @@
 #include QMK_KEYBOARD_H
+#ifdef COMBO_ENABLE
+#   include "combos.h"
+#endif
 
 // Convert 5-bit to 8-bit packed modifiers
 #define MOD_TAP_GET_MOD_BITS(k) (((k) & 0x0f00) >> (((k) & 0x1000) ? 4 : 8))
