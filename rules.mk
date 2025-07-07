7c7b3023edd875d5d5c8f5dcf7d99fa51929727a
ckaadic <ckaadic@gmail.com>
2025-07-07 15:18:42 +0200

    mxbad

--- rules.mk
+++ rules.mk
@@ -28,6 +28,6 @@ ifeq ($(strip $(OLED_ENABLE)), yes)
         OPT_DEFS += -D$(strip $(OLED))
         SRC += oled_icons.c oled_luna.c
     else
-        SRC += oled_icons.c oled_bongocat.c
+        SRC += oled_icons.c oled_mxbad.c
     endif
 endif
